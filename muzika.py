2585ce740a351a638cb7fcac9257f826cf7f8085
walid <walid.sa3d@gmail.com>
2015-07-25 18:47:22 +0100

    colored output, better search results

--- muzika.py
+++ muzika.py
@@ -5,23 +5,21 @@ import sys
 import requests
 from bs4 import BeautifulSoup as bs
 import collections
-from prettytable import PrettyTable
-
+from termcolor import colored
+import argparse
 
 class Pleer:
     def __init__(self):
         self.api_url = 'http://pleer.com/site_api/files/get_url'
-        self.fieldnames = ['#', 'Artist', 'Title', 'Bitrate', 'Size']
         self.headers = { 'User-Agent': 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1'}
 
-    def get_tracks(self,search_string, page):
+    def get_tracks(self,search_string, n):
         loop_counter = 0
         songlist=[]
         fsonglist={}
-        pt = PrettyTable(self.fieldnames)
-        pt.align="l"
-        for x in xrange(1,page+1):
-            url = 'http://pleer.com/search?q={0}&target=tracks&page={1}&quality=best&sort_mode=0&sort_by=0'
+        page=5
+        for x in xrange(1,page):
+            url = 'http://pleer.com/search?q={0}&target=tracks&page={1}&quality=best&sort_mode=0&sort_by=0&onlydata=true'
             url = url.format(search_string, str(x))
             response = requests.get(url,headers=self.headers)
             soup = bs(response.text)
@@ -36,10 +34,16 @@ class Pleer:
                     s["size"] = song.get('size')
                     s['id']=str(song.get('link'))
                     songlist.append(s)
+        return songlist[:n]
+    def show_results(self,songlist):
+        fsonglist={}
         for n,fsong in enumerate([x for x in songlist if "VBR" not in x.get("bitrate")]):
             fsonglist[n]=fsong
-            pt.add_row([n,fsong['artist'],fsong['title'],fsong['bitrate'],fsong['size']])
-        print pt
+            artist=colored(fsong['artist'],'red')
+            title=colored(fsong['title'],'green')
+            bitrate=colored(fsong['bitrate'],'yellow')
+            size=colored(fsong['size'],'blue')
+            print "%s)\t%s -- %s,%s,%s" %(n,artist,title,bitrate,size)
         return fsonglist
 
     def api_call(self,song_id):
@@ -70,18 +74,19 @@ class Pleer:
                     bytes_read += len(chunk)
         self.show_progressbar(file_size, file_size)
 
-    def display_results(self,search_string, page=2):
-        fsonglist= self.get_tracks(search_string, page)
+    def display_results(self,search_string, n):
+        songlist= self.get_tracks(search_string,n)
+        self.show_results(songlist)
         user_prompt = "Enter the song # to download, or press [Enter] to exit:"
         user_choice = raw_input(user_prompt)
         if user_choice:
             while int(user_choice) >=0:
                 user_choice = int(user_choice)
-                song_id = fsonglist[user_choice]['id']
+                song_id = songlist[user_choice]['id']
                 api_response = self.api_call(song_id)
                 download_url = str(api_response['track_link'])
                 print('Song found! Downloading..')
-                file_name = u'{0}-{1}.mp3'.format(fsonglist[user_choice]['artist'],fsonglist[user_choice]['title'])
+                file_name = u'{0}-{1}.mp3'.format(songlist[user_choice]['artist'],songlist[user_choice]['title'])
                 self.download_file(download_url, file_name)
                 print(('\nFile saved as \'{0}\''.format(file_name)))
                 user_choice = raw_input(user_prompt)
@@ -90,11 +95,14 @@ class Pleer:
             sys.exit(1)
                 
     def main(self):
-        search_string =sys.argv[1]
+        parser = argparse.ArgumentParser(usage="-h for full usage")
+        parser.add_argument('query',help='search query',nargs='+')
+        parser.add_argument('-n', dest="maxnumber", help="max number of results",type=int)
+        args = parser.parse_args()
         print('Searching..')
-        search_string = search_string.replace(' ', '+')
+        search_string = " ".join(args.query).replace(' ', '+')
         try:
-            self.display_results(search_string)
+            self.display_results(search_string,args.maxnumber)
         except requests.exceptions.ConnectionError:
             print("[ERROR] Server unreachable!")
 
